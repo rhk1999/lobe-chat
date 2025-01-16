81b238de545f01891c3e75ad55ffc4ef1cc0a331
Arvin Xu <arvinx@foxmail.com>
2025-01-16 07:20:33 +0000

    try to fix cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -57,7 +57,29 @@ const defaultMiddleware = (request: NextRequest) => {
   // console.log('[origin]', url.pathname, '-> [rewrite]', nextPathname);
   url.pathname = nextPathname;
 
-  return NextResponse.rewrite(url);
+  const response = NextResponse.rewrite(url);
+
+  const requestOrigin = request.headers.get('origin');
+
+  if (requestOrigin) {
+    response.headers.set('Access-Control-Allow-Origin', requestOrigin);
+    response.headers.set('Access-Control-Allow-Credentials', 'true');
+    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    response.headers.set(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    );
+
+    // 如果是预检请求，确保返回正确的状态码
+    if (request.method === 'OPTIONS') {
+      return new NextResponse(null, {
+        headers: response.headers,
+        status: 200,
+      });
+    }
+  }
+
+  return response;
 };
 
 const publicRoute = ['/', '/discover'];
